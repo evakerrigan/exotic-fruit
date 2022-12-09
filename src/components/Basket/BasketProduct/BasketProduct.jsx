222ec37efc8db1257c062d62806ee54c756ca2fc
Eva Kerrigan <evakerrigan@gmail.com>
2022-12-09 20:36:34 +0300

    fix basket

--- src/components/Basket/BasketProduct/BasketProduct.jsx
+++ src/components/Basket/BasketProduct/BasketProduct.jsx
@@ -6,7 +6,7 @@ export const BasketProduct = (productId) => {
 
   // console.log("исходный basket =", basket);
 
-  const addProductToBasket = (productId) => {
+  const addProductToBasket = ({productId}) => {
     console.log("ткнули на плюс, productId =", productId);
 
     const initProductCount = basket.products[productId] || 0;
@@ -26,7 +26,7 @@ export const BasketProduct = (productId) => {
     console.log("basket =", basket);
   };
 
-  const removeProductToBasket = (productId) => {
+  const removeProductToBasket = ({productId}) => {
     console.log("ткнули на минус, productId =", productId);
 
     const initProductCount = basket.products[productId] || 0;
